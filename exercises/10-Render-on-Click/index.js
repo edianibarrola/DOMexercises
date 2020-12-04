2aa4b382298388fc7ba00671c55412cd1a254156
edianibarrola <edianibarrola@gmail.com>
2020-12-04 18:54:10 +0000

    update

--- exercises/10-Render-on-Click/index.js
+++ exercises/10-Render-on-Click/index.js
@@ -1,6 +1,8 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
-
+	let newDiv = document.createElement("div");
+	newDiv.innerHTML = "Hello World";
+	newDiv.style.background = "yellow";
+	document.body.appendChild(newDiv);
 });
--- exercises/11-Add-LI-on-Click/index.js
+++ exercises/11-Add-LI-on-Click/index.js
@@ -1,5 +1,9 @@
 let button = document.querySelector("#superDuperButton");
 button.addEventListener("click", function() {
 	//your code here
-
+	var ulElement = document.querySelector("ul");
+	var newLi = document.createElement("li");
+	var liText = document.createTextNode("Fourth element");
+	newLi.appendChild(liText);
+	ulElement.appendChild(newLi);
 });
