41c3a4716c8c6f2fbd8404d42e086a3b6d564d49
edianibarrola <edianibarrola@gmail.com>
2020-12-06 15:55:15 +0000

    update

--- exercises/13-Add-Options-to-the-Select/index.js
+++ exercises/13-Add-Options-to-the-Select/index.js
@@ -2,5 +2,14 @@ window.onload = function() {
 	console.log("The website was loaded...");
 	let countries = ["USA", "France", "Italy", "Brazil", "Colombia", "Belize", "Venezuela"];
 
-  // your code here
+	// your code here
+	var selection = document.querySelector("#mySelect");
+
+	for (let i = 0; i < countries.length; i++) {
+		var newOption = document.createElement("option");
+		newOption.value = countries[i];
+		newOption.innerHTML = countries[i];
+		selection.appendChild(newOption);
+		console.log(newOption);
+	}
 };
